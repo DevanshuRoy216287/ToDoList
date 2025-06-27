3c92ae827374c82bc4718385dedff94ed974cc20
Devanshu Roy <im.devanshuroy@gmail.com>
2025-06-27 18:21:25 +0530

    Update script.js 2

--- script.js
+++ script.js
@@ -11,3 +11,58 @@ setInterval(() => {
         MainText.style.opacity=1;
     }, 1000);
 }, 3000);
+
+const windowTask=document.getElementById("task-window");
+const container=document.getElementById("container");
+
+function AddTasks(y) {
+    if (y==1) {
+        windowTask.style.display="block";
+        container.style.display="block";
+    }
+
+    else {
+        windowTask.style.display="none";
+        container.style.display="none";
+    }
+}
+
+let subjectArray = [];
+let descriptionArray = [];
+let dateArray = [];
+
+function CreateTasks() {
+
+    let subject=document.getElementById("subject").value;
+    let description=document.getElementById("description").value;
+    let date=document.getElementById("date").value;
+
+    if (subject.trim() === "") {
+        alert("Task should contain Subject");
+    }
+
+    else {
+        windowTask.style.display="none";
+        container.style.display="none";
+
+        subjectArray.push(subject);
+        descriptionArray.push(description);
+        dateArray.push(date);
+
+        let index = subjectArray.length - 1;
+        ShowTasks(index);
+    }
+}
+
+function ShowTasks(x) {
+    let taskContainer=document.getElementById("general-tasks");
+
+    let taskButton=document.createElement("button");
+    taskButton.textContent=subjectArray[x];
+    taskButton.onclick = function () {
+        ShowTasksMain(x);
+    };
+    taskButton.className="task-button";
+
+    taskContainer.appendChild(taskButton);
+}
